477b4db4f4b32e665e8c4e6d6886b00a8d96762c
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 18:50:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Simple_form from './Component/Simple_form/Simple_form'
+
+function App() {
+
+
+  return (
+     <div>
+        <Simple_form></Simple_form>
+     </div>
+  )
+}
+
+export default App
