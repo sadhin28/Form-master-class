bf4b11705f50b595c350d37081fb541b07683512
sadhin28 <taosifsadhin113@gmail.com>
2025-05-12 19:55:25 +0600

    add error handeler

--- src/Component/StatefullForm/StatefullForm.jsx
+++ src/Component/StatefullForm/StatefullForm.jsx
@@ -2,14 +2,26 @@ import React, { useState } from 'react';
 
 const StatefullForm = () => {
     const [email,setemail]=useState(null);
-    
+    const [password,setpassword]=useState(null);
+    const [error,seterror]=useState('')
     const hancelSubmit=e=>{
         e.preventDefault()
+        if(password.length <6){
+            seterror('Password must be 6 character or longer')
+        }else{
+            seterror('')
+        }
         console.log(email)
+        console.log(password)
+        
     }
     const handelEmail=e=>{
         e.preventDefault();
         setemail(e.target.value)
+    }
+    const handelPassword = e=>{
+        e.preventDefault();
+        setpassword(e.target.value)
     }
 
     return (
@@ -23,9 +35,12 @@ const StatefullForm = () => {
                                 <label className="label">Email</label>
                                 <input onChange={handelEmail} name="email" type="email" className="input" placeholder="Email" />
                                 <label className="label">Password</label>
-                                <input name="password" type="password" className="input" placeholder="Password" />
+                                <input onChange={handelPassword } name="password" type="password" className="input" placeholder="Password" />
                                 <div><a className="link link-hover">Forgot password?</a></div>
                                 <button className="btn btn-neutral mt-4">Login</button>
+                                {
+                                    error && <p className='text-center'>{error}</p>
+                                }
                                </form>
                             
                         </div>
