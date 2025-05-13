d5b46f8778435ca7a90babb5fe0009e1dcb31661
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 21:34:22 +0600

    add

--- src/Component/SpecialPerson/SpecialPerson.jsx
+++ src/Component/SpecialPerson/SpecialPerson.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { assetContext } from "../GrandPa/GrandPa";
+
 const SpecialPerson = ({asset}) => {
+    const gift = useContext(assetContext)
     return (
         <div>
             <h1>Special Person</h1>
             <p>Has:{asset}</p>
+            <p>Also Has :{gift}</p>
         </div>
     );
 };
