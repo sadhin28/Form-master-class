a60b0c00b15cec27042652cfc603e91353e40cfc
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 19:20:57 +0600

    add rafsan

---
+++ src/Component/Cousin/Cousin.jsx
@@ -0,0 +1,10 @@
+const Cousin = ({name}) => {
+    return (
+        <div className="">
+            
+            <p className="border border-green-500 rounded-2xl p-2">{name}</p>
+        </div>
+    );
+};
+
+export default Cousin;
--- src/Component/Uncle/Uncle.jsx
+++ src/Component/Uncle/Uncle.jsx
@@ -1,7 +1,13 @@
+import Cousin from "../Cousin/Cousin";
+
 const Uncle = () => {
     return (
         <div  className="border border-amber-500 rounded-2xl p-2">
             <h1>Uncle</h1>
+            <div className="grid grid-cols-2 gap-2">
+                <Cousin name={'Rafsan'}></Cousin>
+                <Cousin name={'Rafiya'}></Cousin>
+            </div>
         </div>
     );
 };
