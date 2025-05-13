e3513dbd5847214207db3658052ca8f612a627bc
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 19:11:28 +0600

    add dotter

--- src/Component/Dad/Dad.jsx
+++ src/Component/Dad/Dad.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Myself from '../Myself/Myself';
 import Brother from '../Brother/Brother';
+import Sister from '../Sister/Sister';
 
 const Dad = () => {
     return (
-        <div className=' text-center border border-amber-500 rounded-2xl p-2'>
+        <div className=' text-center border border-amber-500 rounded-2xl p-5'>
             <h1>Father</h1>
            <div className='grid grid-cols-2 gap-2'>
               <div  className="border border-blue-500 rounded-2xl p-2">
@@ -12,6 +13,9 @@ const Dad = () => {
             </div>
             <div  className="border border-green-500 rounded-2xl p-2">
                 <Brother></Brother>
+            </div>
+            <div className="border border-red-500 rounded-2xl p-2">
+                <Sister></Sister>
             </div>
            </div>
         </div>
---
+++ src/Component/Sister/Sister.jsx
@@ -0,0 +1,10 @@
+
+const Sister = () => {
+    return (
+        <div>
+             <h1>Dotter</h1>
+        </div>
+    );
+};
+
+export default Sister;
