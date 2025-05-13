b2620c6ca729d52e59b76eb6ef9dc5295fb96016
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 19:04:40 +0600

    add myself

--- src/Component/Dad/Dad.jsx
+++ src/Component/Dad/Dad.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Myself from '../Myself/Myself';
 
 const Dad = () => {
     return (
         <div className='border border-amber-500 rounded-2xl p-2'>
             <h1>Father</h1>
+            <div  className="border border-blue-500 rounded-2xl p-2">
+                <Myself></Myself>
+            </div>
         </div>
     );
 };
---
+++ src/Component/Myself/Myself.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Myself = () => {
+    return (
+        <div>
+            <h1>Taosif</h1>
+        </div>
+    );
+};
+
+export default Myself;
