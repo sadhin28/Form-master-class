620b2da3f0887069f3a55796895765d97d06861a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 18:58:03 +0600

    add grand pa component

--- src/Component/GrandPa/GrandPa.jsx
+++ src/Component/GrandPa/GrandPa.jsx
@@ -4,10 +4,14 @@ import Uncle from "../Uncle/Uncle";
 
 const GrandPa = () => {
     return (
-        <div>
-         <Dad></Dad>
-         <Uncle></Uncle>
-         <Anti></Anti>
+        <div className=" border-2 m-3 p-10 rounded-2xl">
+            <h1 className="mb-10">GrandPapa</h1>
+            <div className="flex gap-3">
+         
+            <Dad></Dad>
+            <Uncle></Uncle>
+             <Anti></Anti>
+            </div>
         </div>
     );
 };
