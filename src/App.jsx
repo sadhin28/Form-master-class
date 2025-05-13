30997006d71a5dd6e3b8def8268d9b45cece12be
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 18:50:04 +0600

    add all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import Anti from './Component/Anti/Anti'
+import Dad from './Component/Dad/Dad'
 import RefForm from './Component/RefForm/RefForm'
 import Simple_form from './Component/Simple_form/Simple_form'
 import StatefullForm from './Component/StatefullForm/StatefullForm'
+import Uncle from './Component/Uncle/Uncle'
 import HookForm from './Custom-hooks-form/HookForm'
 
 function App() {
@@ -12,7 +15,10 @@ function App() {
         {/* <Simple_form></Simple_form> */}
          {/* <StatefullForm></StatefullForm> */}
          {/* <RefForm></RefForm> */}
-         <HookForm></HookForm>
+         {/* <HookForm></HookForm> */}
+         <Dad></Dad>
+         <Uncle></Uncle>
+         <Anti></Anti>
      </div>
   )
 }
