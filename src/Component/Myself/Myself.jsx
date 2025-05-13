85b114af9a7fdf87d522ae1870117792b2e6b5be
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 19:35:08 +0600

    add special person

--- src/Component/Myself/Myself.jsx
+++ src/Component/Myself/Myself.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SpecialPerson from '../SpecialPerson/SpecialPerson';
 
 const Myself = () => {
     return (
         <div>
-            <h1>Son 1</h1>
+            <h1>Me</h1>
+            <div className="border border-green-500 rounded-2xl p-2">
+                 <SpecialPerson></SpecialPerson>
+            </div>
         </div>
     );
 };
---
+++ src/Component/SpecialPerson/SpecialPerson.jsx
@@ -0,0 +1,9 @@
+const SpecialPerson = () => {
+    return (
+        <div>
+            <h1>Special Person</h1>
+        </div>
+    );
+};
+
+export default SpecialPerson;
