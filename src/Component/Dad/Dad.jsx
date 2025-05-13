caab13bae97771ba8a5bff520ba20e02c0702914
sadhin28 <taosifsadhin113@gmail.com>
2025-05-13 19:23:05 +0600

    add cousign

--- src/Component/Dad/Dad.jsx
+++ src/Component/Dad/Dad.jsx
@@ -6,7 +6,7 @@ import Sister from '../Sister/Sister';
 const Dad = () => {
     return (
         <div className=' text-center border border-amber-500 rounded-2xl p-5'>
-            <h1>Father</h1>
+            <h1 className='font-black'>Father</h1>
            <div className='grid grid-cols-2 gap-2'>
               <div  className="border border-blue-500 rounded-2xl p-2">
                 <Myself></Myself>
--- src/Component/Uncle/Uncle.jsx
+++ src/Component/Uncle/Uncle.jsx
@@ -2,8 +2,8 @@ import Cousin from "../Cousin/Cousin";
 
 const Uncle = () => {
     return (
-        <div  className="border border-amber-500 rounded-2xl p-2">
-            <h1>Uncle</h1>
+        <div  className=" text-center border border-amber-500 rounded-2xl p-2">
+            <h1 className="mt-3 font-black">Uncle</h1>
             <div className="grid grid-cols-2 gap-2">
                 <Cousin name={'Rafsan'}></Cousin>
                 <Cousin name={'Rafiya'}></Cousin>
